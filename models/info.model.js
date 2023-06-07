eabc8159758b42db9be52d81f679f47f49d06e88
edend85 <117260021+edend85@users.noreply.github.com>
2023-06-07 10:48:45 +0300

    Update info.model.js

--- models/info.model.js
+++ models/info.model.js
@@ -2,23 +2,25 @@ const DB = require('../utils/db');
 
 class Info{
 
-    infoId;
+    title;
     description;
-    constructor(infoId,description){
-        this.infoId = infoId;
+    constructor(title,description){
+        this.title = title;
         this.description = description;
     }
 
-    static async ShowAllInfo() {
-        return await new DB().ShowAllInfo('info');
+    static async ShowAllQuerys() {
+        return await new DB().ShowAllQuerys('Information');
     }
-    static async AddlInfo(doc) {
-        return await new DB().AddlInfo('info',doc);
+    static async AddQuery(title,description) {
+        this.title = title
+        this.description = description
+        return await new DB().AddQuery('Information',{...this});
     }
-    static async UpdateInfo(id,des) {
+    static async UpdateQuery(id,des) {
         let query = {"infoId":{$eq:id}}
         let desUpdate = ({description},{$set:des});
-        return await new DB().UpdateInfo('info',query,desUpdate);
+        return await new DB().UpdateQuery('Information',query,desUpdate);
     }
 }
 module.exports = Info;
