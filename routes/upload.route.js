c0ae6cfe9b30e256cfb9e1695cd9c32d7f807f1d
Nir Izhak <nir676676@gmail.com>
2023-07-11 13:11:48 +0300

    Update upload.route.js

--- routes/upload.route.js
+++ routes/upload.route.js
@@ -15,7 +15,7 @@ UploadRoute.post("/", async (req, res) => {
     try {
         let {image} = req.body;
         let imageStr = `data:image/jpg;base64,${image}`;
-        cloudinary.uploader.upload_large(imageStr, {width:150, crop: "scale"}, (err, result)=>{
+        cloudinary.uploader.upload_large(imageStr, {quality: "auto", fetch_format: "auto"}, (err, result)=>{
             if(err) throw new Error(err)
             res.status(200).json(result)
         })
