f9d1d297bd22ef0cab0f32d6026f92db1bc687b8
edend85 <117260021+edend85@users.noreply.github.com>
2024-03-14 17:03:21 +0200

    Update db.js

--- utils/db.js
+++ utils/db.js
@@ -20,7 +20,7 @@ class DB {
       async connect() {
         try {
           await this.client.connect();
-          console.log('Connected to the database');
+          /*console.log('Connected to the database');*/
         } catch (error) {
           console.error('Failed to connect to the database:', error);
         }
