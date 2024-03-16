de52185d60fb9bad5d835f180e7a1ac375e5e9b9
edend85 <117260021+edend85@users.noreply.github.com>
2024-03-16 11:28:19 +0200

    Update users.route.js

--- routes/users.route.js
+++ routes/users.route.js
@@ -59,7 +59,7 @@ UsersRoutes.post('/Login', async (req, res) => {
     try {
         let {email,password} = req.body;
         let user = await UserModel.FindbyEmail(email);
-        if(!user){console.log('user :>> ', user); return user}
+        if(!user){res.status(400)}
         console.log('user :>> ', user);
         let result = await UserModel.Login(user.password,password);
         if(result){
